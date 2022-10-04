65644996846c2c7a6641760e953cd6836617a95a
Curtis Neveu <62405245+curtisneveu@users.noreply.github.com>
2022-10-04 16:20:37 -0500

    minor edits

--- readbatch.m
+++ readbatch.m
@@ -14,32 +14,62 @@ function [values, params] = readbatch(fname)
 
 
 fid = fopen(fname);
+if fid<0
+    error('file not found')
+end
 
 values = zeros(0,1);
 params = strings(0,4);
 parn = ["Ivd","Ics","Ies";...
         "gE" ,"gE" ,"gg"];
 pnum = 0;
+auto = false;
+isneu = false;
+nparams = ["threshold","spikdur","vmint","cm"];
 while 1==1
     tline = fgets(fid);
+    disp(tline)
     if tline==-1;break;end
     
     if contains(tline,'PARAMETER')
         pnum = pnum + 1;
         cnt = 1;
-    elseif contains(tline,'One value')
-        values(cnt,pnum) = double(string(regexp(tline,'[0-9.]+','match')));
-        cnt = cnt+1;
+        auto = false;
     elseif contains(tline,'File containing')
         out = string(regexp(tline,'\S+','match'));
         out = out(contains(out,'/') & contains(out,'.'));
         params(pnum,1) = out;
+        isneu = contains(tline,'.neu');
     elseif contains(tline,'keyword in formula')
         params(pnum,2) = string(regexp(tline,'(Ivd|Ics|Ies)','match'));
+    elseif contains(tline,'threshold')
+        val = double(string(regexp(tline,'[0-9.]+','match'))) + 1;
+        params(pnum,3) = nparams(val(1));
     elseif contains(tline,'which parameter')
         params(pnum,3) = string(regexp(tline,'[0-9.]+','match'));
         keyw = parn{2,contains(parn(1,:),params(pnum,2))};
         params(pnum,4) = keyw(double(params(pnum,3))+1);
+    elseif contains(tline,'Manual')
+        val = string(regexp(tline,'[01]','match'))=="1";
+        auto = val(1);
+    elseif auto
+        if contains(tline,'Start')
+            val = double(string(regexp(tline,'[0-9.]+','match')));
+            start = val(1);
+        elseif contains(tline,'Increment')
+            val = double(string(regexp(tline,'[0-9.]+','match')));
+            incr = val(1);
+        elseif contains(tline,'Stop value')
+            val = double(string(regexp(tline,'[0-9.]+','match')));
+            stop = val(1);
+            vals = start:incr:stop;
+            values(1:length(vals),pnum) = vals;
+        end
+    elseif ~auto
+        if contains(tline,'One value')
+            values(cnt,pnum) = double(string(regexp(tline,'[0-9.]+','match')));
+            cnt = cnt+1;
+        end
     end
 end
 
